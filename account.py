5d07169f67d7ecc6b8b6574c88c380c9bbc911f4
Affan Bin Inam <100516781+IFFI-aka-AFFAN@users.noreply.github.com>
2024-02-27 09:58:58 -0800

    Update account.py

--- account.py
+++ account.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials
 from firebase_admin import auth
 
 cred = credentials.Certificate("rick-reels-bc13f133cf7c.json")
-#firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred)
 
 
 def app():
